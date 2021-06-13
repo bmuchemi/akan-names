8236fb08f0f3da9810755cdc45ea35294f0334fc
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-06-13 18:18:45 +0300

    add variables to .js file.

--- css/script.js
+++ css/script.js
@@ -24,3 +24,45 @@ function akan(){
         "Afua",
         "Ama"
       ];
+      var B = new Date(month+'/'+date+'/'+year);
+      var akanday = B.getDay()
+  console.log(akanday);
+  if (akanday == 0)
+  {
+      Day = "Sunday";
+  }
+  else if (akanday == 1)
+  {
+      Day = "Monday";
+  }
+  else if (akanday == 2)
+  {
+      Day = "Tuesday";
+  }
+  else if (akanday == 3)
+  {
+      Day = "Wednesday";
+  }
+  else if (akanday == 4)
+  {
+      Day = "Thursday";
+  }
+  else if (akanday == 5)
+  {
+      Day = "Friday";
+  }
+  else if (akanday == 6)
+  {
+      Day = "Saturday";
+  }
+
+  if(date<0 || date>31){
+    alert("Please Enter a valid day")
+  }
+  if(month<0 || month>12){
+    alert("Please Enter a valid Month")
+  }
+  if(year<1900 || year>2022){
+      alert("Is that really your age?")
+  }
+}
