d2723fb5a03dc1d9f680a650197ffe99f231dd03
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-06-13 17:48:55 +0300

    add function to javascript.js.

---
+++ css/script.js
@@ -0,0 +1,6 @@
+function callingName(){
+    var year = parseInt(document.getElementById("year").value);
+    var month = parseInt(document.getElementById("month").value);
+    var date = parseInt(document.getElementById("date").value);
+    var male = document.getElementById("male").value;
+    var female = document.getElementById("female").value;
