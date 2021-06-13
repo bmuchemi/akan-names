f83992e5d7b09010c85792a44ddd5b235c19564c
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-06-13 17:53:46 +0300

    add variables to javascript.js.

--- css/script.js
+++ css/script.js
@@ -1,6 +1,26 @@
-function callingName(){
+function akan(){
     var year = parseInt(document.getElementById("year").value);
     var month = parseInt(document.getElementById("month").value);
     var date = parseInt(document.getElementById("date").value);
     var male = document.getElementById("male").value;
     var female = document.getElementById("female").value;
+
+
+    var maleNames = [
+        "Kwasi",
+        "Kwadwo",
+        "Kwabena",
+        "Kwaku",
+        "Yaw",
+        "Kofi",
+        "Kwame"
+      ];
+      var femaleNames = [
+        "Akosua",
+         " Adwoa",
+        "Abenaa",
+        "Akua",
+        "Yaa",
+        "Afua",
+        "Ama"
+      ];
