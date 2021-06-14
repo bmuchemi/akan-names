94e762443797c0bcfaf438b2bbd07b94e07c23d9
benjamin muchemi <benjamin.muchemi@student.moringaschool.com>
2021-06-14 09:04:24 +0300

    change position of variables to above the  function.

--- css/script.js
+++ css/script.js
@@ -1,7 +1,36 @@
+var femaleNames = [
+    "Akosua",
+    "Adwoa",
+    "Abenaa",
+    "Akua",
+    "Yaa",
+    "Afua",
+    "Ama"
+];
+
+    var maleNames = [
+        "Kwasi",
+        "Kwadwo",
+        "Kwabena",
+        "Kwaku",
+        "Yaw",
+        "Kofi",
+        "Kwame"
+];
+var dayOfTheWeek = [
+    'Sunday',
+    'Monday',
+    'Tuesday', 
+    'Wednesday', 
+    'Thursday', 
+    'Friday', 
+    'Saturday'
+];
+
 
 function calculate(){
 
-    var year = patseint(document.getElementById("year")).value;
+    var year = parseint(document.getElementById("year")).value;
     var month = parseint(document.getElementById("month")).value;
     var date = parseint(document.getElementById("date")).value;
     
@@ -9,36 +38,7 @@ function calculate(){
     var date = new Date(year + "/" + month + "/" + day);
     var big = date.getDay(); 
 
-    var femaleNames = [
-        'Akosua',
-        'Adwoa',
-        'Abenaa',
-        'Akua',
-        'Yaa',
-        'Afua',
-        'Ama'
-];
-
-        var maleNames = [
-            "Kwasi",
-            "Kwadwo",
-            "Kwabena",
-            "Kwaku",
-            "Yaw",
-            "Kofi",
-            "Kwame"
-];
-    var dayOfTheWeek = [
-        'Sunday',
-        'Monday',
-        'Tuesday', 
-        'Wednesday', 
-        'Thursday', 
-        'Friday', 
-        'Saturday'
-];
-
-
+    
 if (gender === "female") {
     alert("Your name is: " + femaleNames[big] + "and you were born on " + dayOfTheWeek[big])
 } else if (gender === "male") {
